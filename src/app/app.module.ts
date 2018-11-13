e88810ad554e1216acbbc499dde060f7d9d379c2
Jairo Emilio Bautista Mora <jairo.bautista@infometrix.co>
2018-11-13 15:07:32 -0500

    routing, modulo principal y helpers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FugaModule } from './fuga/fuga.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FugaModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
