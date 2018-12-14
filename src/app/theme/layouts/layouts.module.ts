453213e47cd962df1cb9ac0b21b98d8fd8123fd9
Jairo Emilio Bautista Mora <jairo.bautista@infometrix.co>
2018-12-14 12:27:17 -0500

    org files for services and layouts modules

--- src/app/theme/layouts/layouts.module.ts
+++ src/app/theme/layouts/layouts.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { NavigationComponent } from './navigation/navigation.component';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
   ],
-  declarations: []
+  exports: [
+    NavigationComponent
+  ],
+  declarations: [
+    NavigationComponent
+  ],
+
 })
 export class LayoutsModule { }
